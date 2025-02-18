66b165a7b26fdae3f0deca1570cf900e379ef00e
jiraphat.ba@kkumail.com <jiraphat.ba@kkumail.com>
2025-02-18 13:01:33 +0700

    last fornt

--- Univibe/src/component/Content/Content1.jsx
+++ Univibe/src/component/Content/Content1.jsx
@@ -5,14 +5,93 @@ export default function Content() {
   return (
     <section id = "content">
       <div className='header'>
-        <div className='toppig'>Popular</div>
+        <div className='topic'>Popular Event</div>
       </div>
-      <div className='allcard'>
-        <div className='card1'>1</div>
-        <div className='card2'>2</div>
-        <div className='card3'>3</div>
-        <div className='card4'>4</div>
-        <div className='card5'>5</div>
+      <div className='allcardPopular'>
+        <div className='card'>
+          <div className='photo'>
+            <img src="https://placehold.co/250x400" alt="" />
+          </div>
+          <div className='date'>
+            23/2/2002
+          </div>
+          <div className='Name'>
+            ต้อนรับน้องใหม่ 68
+          </div>
+          <div className='Locate'>
+            สนามกีฬา
+          </div>
+        </div>
+        <div className='card'>
+        <div className='photo'>
+            <img src="https://placehold.co/250x400" alt="" />
+          </div>
+          <div className='date'>
+            23/2/2002
+          </div>
+          <div className='Name'>
+            ต้อนรับน้องใหม่ 68
+          </div>
+          <div className='Locate'>
+            สนามกีฬา
+          </div>
+        </div>
+        <div className='card'>
+        <div className='photo'>
+            <img src="https://placehold.co/250x400" alt="" />
+          </div>
+          <div className='date'>
+            23/2/2002
+          </div>
+          <div className='Name'>
+            ต้อนรับน้องใหม่ 68
+          </div>
+          <div className='Locate'>
+            สนามกีฬา
+          </div>
+        </div>
+        <div className='card'>
+        <div className='photo'>
+            <img src="https://placehold.co/250x400" alt="" />
+          </div>
+          <div className='date'>
+            23/2/2002
+          </div>
+          <div className='Name'>
+            ต้อนรับน้องใหม่ 68
+          </div>
+          <div className='Locate'>
+            สนามกีฬา
+          </div>
+        </div>
+        <div className='card'>
+        <div className='photo'>
+            <img src="https://placehold.co/250x400" alt="" />
+          </div>
+          <div className='date'>
+            23/2/2002
+          </div>
+          <div className='Name'>
+            ต้อนรับน้องใหม่ 68
+          </div>
+          <div className='Locate'>
+            สนามกีฬา
+          </div>
+        </div>
+        <div className='card'>
+        <div className='photo'>
+            <img src="https://placehold.co/250x400" alt="" />
+          </div>
+          <div className='date'>
+            23/2/2002
+          </div>
+          <div className='Name'>
+            ต้อนรับน้องใหม่ 68
+          </div>
+          <div className='Locate'>
+            สนามกีฬา
+          </div>
+        </div>
       </div>
       </section>
   )
