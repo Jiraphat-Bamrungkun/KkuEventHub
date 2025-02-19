4832fb3c56c079691dc08f04e5337553bfa93532
jiraphat.ba@kkumail.com <jiraphat.ba@kkumail.com>
2025-02-19 13:12:09 +0700

    update fornt

---
+++ Univibe/src/component/Content/Content2.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import './ContentStyle.css'
+
+export default function Content2() {
+  return (
+    <section id = "content2">
+      <div className='header'>
+        <div className='topic'>Upoming Event</div>
+      </div>
+      <div className='allcardPopular'>
+        <div className='card'>
+          <div className='photo'>
+            <img src="https://placehold.co/250x400" alt="" />
+          </div>
+          <div className='date'>
+            23/2/2002
+          </div>
+          <div className='Name'>
+            ต้อนรับน้องใหม่ 68
+          </div>
+          <div className='Locate'>
+            สนามกีฬา
+          </div>
+        </div>
+        <div className='card'>
+        <div className='photo'>
+            <img src="https://placehold.co/250x400" alt="" />
+          </div>
+          <div className='date'>
+            23/2/2002
+          </div>
+          <div className='Name'>
+            ต้อนรับน้องใหม่ 68
+          </div>
+          <div className='Locate'>
+            สนามกีฬา
+          </div>
+        </div>
+        <div className='card'>
+        <div className='photo'>
+            <img src="https://placehold.co/250x400" alt="" />
+          </div>
+          <div className='date'>
+            23/2/2002
+          </div>
+          <div className='Name'>
+            ต้อนรับน้องใหม่ 68
+          </div>
+          <div className='Locate'>
+            สนามกีฬา
+          </div>
+        </div>
+        <div className='card'>
+        <div className='photo'>
+            <img src="https://placehold.co/250x400" alt="" />
+          </div>
+          <div className='date'>
+            23/2/2002
+          </div>
+          <div className='Name'>
+            ต้อนรับน้องใหม่ 68
+          </div>
+          <div className='Locate'>
+            สนามกีฬา
+          </div>
+        </div>
+        <div className='card'>
+        <div className='photo'>
+            <img src="https://placehold.co/250x400" alt="" />
+          </div>
+          <div className='date'>
+            23/2/2002
+          </div>
+          <div className='Name'>
+            ต้อนรับน้องใหม่ 68
+          </div>
+          <div className='Locate'>
+            สนามกีฬา
+          </div>
+        </div>
+        <div className='card'>
+        <div className='photo'>
+            <img src="https://placehold.co/250x400" alt="" />
+          </div>
+          <div className='date'>
+            23/2/2002
+          </div>
+          <div className='Name'>
+            ต้อนรับน้องใหม่ 68
+          </div>
+          <div className='Locate'>
+            สนามกีฬา
+          </div>
+        </div>
+      </div>
+      </section>
+  )
+}
--- Univibe/src/layout/Mainlayout.jsx
+++ Univibe/src/layout/Mainlayout.jsx
@@ -1,4 +1,5 @@
 import Content from "../component/Content/Content1";
+import Content2 from "../component/Content/Content2";
 import Nav from "../component/Nav/Nav";
 import Promote from "../component/Promote/Promote";
 
@@ -8,6 +9,7 @@ function Mainlayout() {
     <Nav></Nav>
     <Promote></Promote>
     <Content></Content>
+    <Content2></Content2>
   </section>);
 }
 export default Mainlayout
