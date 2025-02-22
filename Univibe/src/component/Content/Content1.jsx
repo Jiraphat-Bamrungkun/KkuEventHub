8ca6d8d7bc4b8e96666fc8a817bd6218fee9fff8
Sorawas-Laokhom <sorawas.l@kkumail.com>
2025-02-23 01:33:36 +0700

    1

--- Univibe/src/component/Content/Content1.jsx
+++ Univibe/src/component/Content/Content1.jsx
@@ -6,9 +6,9 @@ export default function Content() {
   return (
     <section id = "content">
       <div className='header'>
-        <div className='topic'>Popular Event</div>
+        <div className='topic'>Recent Event</div>
       </div>
-      <div className='allcardPopular'>
+      <div className='allcardRecent'>
         <Link to="/detail" className='card'>
           <div className='photo'>
             <img src="https://placehold.co/250x400" alt="" />
