d698010c2dc3faed681a807cc52d2136d227107e
jiraphat.ba@kkumail.com <jiraphat.ba@kkumail.com>
2025-02-23 13:35:24 +0700

    update last 23/2

--- Univibe/src/component/Content/Content2.jsx
+++ Univibe/src/component/Content/Content2.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './ContentStyle.css'
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 export default function Content2() {
   return (
-    <section id = "content2">
+    <section id="content2">
       <div className='header'>
         <div className='topic'>Upoming Event</div>
       </div>
@@ -13,31 +14,28 @@ export default function Content2() {
             <img src="https://s3-alpha-sig.figma.com/img/da85/7b7e/895e9cf45921b0a7677dabff3fdf8562?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=gIOREt4-z4UJKiXqv9skH8-~Vrgxs1Vetpzx27tmCSYRZMHwLK6QuEwcwcnKjXlSe8FYXNYNAWL3T-MRLvv-pY5fe-CCuNewEb0P1E4N77zCMUzWnrDH6JEZjYjN-J-gH3SlbTMvgYxf2OhgC4v7hxoviGjBOzN2idWB1I5~TCiaM4g7nwsZbvNXAJKv7F3eR6JWdNydf9QENHwITOs4E3jLTp8gAlHvMCRkLNURuWnv0WiP9K0BhHT-nRZ4JdYfzA385v2384xXOUErO0Md0zHym~T-qUwmLyZQpvH37wQdDB0a~QIJRX0S43Z85NDf7O-esmB686YiCJLj~fzWFg__" alt="" />
           </div>
           <div className='date'>
-            23/2/2002
-          </div>
+          อ, 23 กุมภาพันธ์          </div>
           <div className='Name'>
-            ต้อนรับน้องใหม่ 68
-          </div>
+          KKU RACE TO LOVE          </div>
           <div className='Locate'>
-            สนามกีฬา
+            <LocationOnIcon style={{ color: '#888' }} />
+            หน้าคุ้มสีฐาน
           </div>
         </div>
         <div className='card'>
-        <div className='photo'>
+          <div className='photo'>
             <img src="https://s3-alpha-sig.figma.com/img/95cd/8fef/e0a6d6d182c0e174d6260ca0aee19b7b?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=mKGk3ZWkBNhKW0k23r1ysHVe~5f82YYS4GfiFzR-7shjd4yGKoX2yNs8rjKtoo6Wyic1n37xXFgC6WXxaBj-Lu3C1uPGZl3nxxnwTrwzg1xhLwCiYJ~J6jcC6tjyiTgIwAUuSZ9-LJWlzpLiSWz4NQSW0zwP05Zwk2xyXv64Qg7bO1nv~RChPz8abi9sbuYpvoJJT9063q0N44mVoknOfbFG2QCRJXqGDK6SkZWB-EjGUJumdFK-q6nklchO1Bwe6LX7UUJSvNR3LJ6Da1OOVcfXEc~9EYAN-LQogLUa2P6MphtlRKuhT2cF7hQAZL6Zj3XeBEy4PPvsxW1W4ZVgXQ__" alt="" />
           </div>
           <div className='date'>
-            23/2/2002
-          </div>
+            ศ, 14 ก.พ - อา, 16 ก.พ          </div>
           <div className='Name'>
-            ต้อนรับน้องใหม่ 68
-          </div>
+            FOOD FEST 2025 by TE KKU          </div>
           <div className='Locate'>
-            สนามกีฬา
-          </div>
+            <LocationOnIcon style={{ color: '#888' }} />
+            ลานบริเวณหน้า คณะเทคโนโลยี          </div>
         </div>
         <div className='card'>
-        <div className='photo'>
+          <div className='photo'>
             <img src="https://s3-alpha-sig.figma.com/img/f9f0/4b46/78de848caead04e9cda5a391fb7dd268?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=qr7y4r-6yYD0QmyZ0D8siMTPmi6hUQ7NGPM1OhXN5vLCxHOQWlojs73hxALvbDs-2Dekx4x6jt41PsO08LXC1uzKwWpi26bU98CNOaGn9-aCu4c4UqsLZVZ~~VMONyd9apN1~vrtGwIBFP7sAfewST45h2zg0s4Xk4u36y0zsdXfkV64rBrSFLDWLm~1czxQUcDt~0Wsy7mNVOayQfZLRRHiWcQq7WnPfyWty7dxjNTLFYmcOwo40j02uNeSnee77SMmrhMnB9UxZtBqgzWUvUUt-CKP3MDOZddIPQ0QGh39cMV16FSr9UOe56MWJB3bU3T06v~I-EAxyrDixhBHyw__" alt="" />
           </div>
           <div className='date'>
@@ -47,39 +45,40 @@ export default function Content2() {
             ต้อนรับน้องใหม่ 68
           </div>
           <div className='Locate'>
+            <LocationOnIcon style={{ color: '#888' }} />
             สนามกีฬา
           </div>
         </div>
         <div className='card'>
-        <div className='photo'>
+          <div className='photo'>
             <img src="https://s3-alpha-sig.figma.com/img/b319/2a1c/5c7e4eda1ebf25fc685b703fd76af666?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=lFO3nyt9MXpfFmj6PukQiXg-SJF99qfL4Be6cvlBmaXeT9XYBuuGloMM9IZy9~kVPLcB9CGPGZ~YBey2~BHtzcatl4d9cOuS-NBRzEepAVevh4YHaenohHYBSba1lEOASmdGgxSSbiIfeOSRx1WBf21kv1243pict6eogTl7hMSqhvuEY~XWLxKcGHlec4ELg8T0lsd6mIreMXR8-sIeYR9t6uJQNtKG29oyQvHpcNDoML1LX-x7m98zs8NG4EuY22lHsxb1s7RzpYd2IT23Pwu5sDsk7zRL8LeXGw1W8CEEni4NxEwU8Xlwpw~rhFNzAe-QBu-~RxxQRjsovueReQ__" alt="" />
           </div>
           <div className='date'>
-            23/2/2002
-          </div>
+            อ, 25 ก.พ - ส, 1 มี.ค           </div>
           <div className='Name'>
-            ต้อนรับน้องใหม่ 68
-          </div>
+            มาเด้อ ขวัญเอ้ย          </div>
           <div className='Locate'>
-            สนามกีฬา
+            <LocationOnIcon style={{ color: '#888' }} />
+            คณะสถาปัตยกรรมศาสตร์
           </div>
         </div>
         <div className='card'>
-        <div className='photo'>
+          <div className='photo'>
             <img src="https://s3-alpha-sig.figma.com/img/fe0d/f673/83f59047861a406376d5fbb76e199442?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=gW9QtQoLaIAK-2gTT95-BKgpid7CZFoXGWYJxloAyhcu28XN99ue-DyzQaNLmcedWCSdtHKVBzVv6mzuNZZuCjfLkirISYEEWhrg5IzHH8t5ebzsiPxY2NlN-QJMMFFk0h7LwafHC3e4SZOHGa~h4-OO~xkk-g8KqlpRiZwcxgau4byOdPLEP2QbG08t0dxbsTArrNGgILfQb9dWL3v5ORvmA0sMF2zk57RLlnJIr-zQUR9hJL4adaHr~uiMQyOFnsBAhn5QVmFWbsQMWX~tk95fqw52mJt4o-t-cyFcMGm~IE2OlmB2NIJknftaG6EO26vWoxi5HvlMiN5BiXfvxw__" alt="" />
           </div>
           <div className='date'>
-            23/2/2002
+            ส, 15 ก.พ - ส, 22 ก.พ
           </div>
           <div className='Name'>
-            ต้อนรับน้องใหม่ 68
+            KKU GAME 2024
           </div>
           <div className='Locate'>
-            สนามกีฬา
+            <LocationOnIcon style={{ color: '#888' }} />
+            สนามกีฬากลาง 50 ปี
           </div>
         </div>
         <div className='card'>
-        <div className='photo'>
+          <div className='photo'>
             <img src="https://s3-alpha-sig.figma.com/img/3709/a164/08154e98caa44b0181ad92512842a4d6?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=JM-56~uTR6-Q2uVyudaIMqiG0jDyM7UBdORlbb7w9vhdLJ82L1fWahsArFdNgrF4ANIiPO7~nT5rtF9lyqCHujJLLxaL1WLK5atgoWLo6Bzf6zpXafWFWFTZHJudx5t5EzuYf99SjplATCj2eibzqZKVmYsRA6rgpZmVcIvTV3r~JG8GmmFW2NWLitXi32jzhqCUEu0~wuzo-Re8nEwnhOQYE2uCWW0Tb9rZm5GwCvwarDoGFpYiqpUlSWgpkj7LN1megMzghk106K1RDEIW6IAd82pZ2POHvcG9Z7f4wBSO~fHp1FXEmyxlb412f2nFqq9GP--MaS9dWevlWyRtFg__" alt="" />
           </div>
           <div className='date'>
@@ -89,10 +88,11 @@ export default function Content2() {
             ต้อนรับน้องใหม่ 68
           </div>
           <div className='Locate'>
+            <LocationOnIcon style={{ color: '#888' }} />
             สนามกีฬา
           </div>
         </div>
       </div>
-      </section>
+    </section>
   )
 }
