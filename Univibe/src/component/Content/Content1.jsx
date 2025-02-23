c138607b9d4306ed2faf57eb02849870af882a34
jiraphat.ba@kkumail.com <jiraphat.ba@kkumail.com>
2025-02-23 12:32:33 +0700

    new update webpic

--- Univibe/src/component/Content/Content1.jsx
+++ Univibe/src/component/Content/Content1.jsx
@@ -11,7 +11,7 @@ export default function Content() {
       <div className='allcard'>
         <Link to="/detail" className='card'>
           <div className='photo'>
-            <img src="https://placehold.co/250x400" alt="" />
+            <img src="https://scontent.fbkk12-1.fna.fbcdn.net/v/t39.30808-6/473743497_1064256355501322_1982394039556270513_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=127cfc&_nc_ohc=jUgW2LR0rVYQ7kNvgH9h-j1&_nc_zt=23&_nc_ht=scontent.fbkk12-1.fna&_nc_gid=AB3mkqWv2wQ2eCryF7TcZok&oh=00_AYAHhyDM3q6u_oUkhhpMjUsFZAsDYIXpNYxLm5cVRYz2WA&oe=67C076F1" alt="" />
           </div>
           <div className='date'>
             23/2/2002
@@ -25,7 +25,7 @@ export default function Content() {
         </Link>
         <div className='card'>
         <div className='photo'>
-            <img src="https://placehold.co/250x400" alt="" />
+            <img src="https://scontent.fbkk12-4.fna.fbcdn.net/v/t39.30808-6/480570316_601017649405389_573678911040515669_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=127cfc&_nc_ohc=S6twc8mT4UEQ7kNvgFNdsau&_nc_zt=23&_nc_ht=scontent.fbkk12-4.fna&_nc_gid=APKIrZuACjxtlfB--pCf6YV&oh=00_AYDoxkZeMLSsan8ociMRDpR_xnfEbjUOtqJnaCYETYilDA&oe=67C09504" alt="" />
           </div>
           <div className='date'>
             23/2/2002
@@ -39,7 +39,7 @@ export default function Content() {
         </div>
         <div className='card'>
         <div className='photo'>
-            <img src="https://placehold.co/250x400" alt="" />
+            <img src="https://scontent.fbkk12-3.fna.fbcdn.net/v/t39.30808-6/476296472_1175952014534483_2074529046553867935_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=833d8c&_nc_ohc=8Im5tIqaIwQQ7kNvgFz7CGV&_nc_zt=23&_nc_ht=scontent.fbkk12-3.fna&_nc_gid=ACP9dmfqQoRnvTUNyQgxSFW&oh=00_AYBF26UesQcgueso2c_hg3UnIUgAOBhUZJmV0rWyUcbjIw&oe=67C06D45" alt="" />
           </div>
           <div className='date'>
             23/2/2002
@@ -53,7 +53,7 @@ export default function Content() {
         </div>
         <div className='card'>
         <div className='photo'>
-            <img src="https://placehold.co/250x400" alt="" />
+            <img src="https://scontent.fbkk12-3.fna.fbcdn.net/v/t39.30808-6/476637684_1184884320305259_8007419021165002206_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=833d8c&_nc_ohc=P_GXthYSnxMQ7kNvgGJh7ZC&_nc_zt=23&_nc_ht=scontent.fbkk12-3.fna&_nc_gid=AJ6f9Mg286iW9AEzaDNbpSG&oh=00_AYCWWNRDZ8lL7uiotaMLqLiz588TsB9fYqMFubXJbqQdkg&oe=67C06CD5" alt="" />
           </div>
           <div className='date'>
             23/2/2002
@@ -67,7 +67,7 @@ export default function Content() {
         </div>
         <div className='card'>
         <div className='photo'>
-            <img src="https://placehold.co/250x400" alt="" />
+            <img src="https://scontent.fbkk12-4.fna.fbcdn.net/v/t39.30808-6/473389476_1170714325056995_3407120416909351190_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=833d8c&_nc_ohc=pV9VFjb2UM0Q7kNvgHgDoPL&_nc_zt=23&_nc_ht=scontent.fbkk12-4.fna&_nc_gid=ABkHD9urmP3Kk4BsBpQiIq8&oh=00_AYDQiO-nadyL2zJMcvtSs8h0p5CAInyql5lQnoWcAcK5Pw&oe=67C0733B" alt="" />
           </div>
           <div className='date'>
             23/2/2002
@@ -81,7 +81,7 @@ export default function Content() {
         </div>
         <div className='card'>
         <div className='photo'>
-            <img src="https://placehold.co/250x400" alt="" />
+            <img src="https://s3-alpha-sig.figma.com/img/24f1/2179/5176d5afb5c916ab5b9efac569b0e7d1?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=mp3pSnuCwW1okIN6~PP6oHE2CYzRK-yDmdgAeeqDn-XDPG483aCoiGEz73D5oCHFMesuHFaovtIRLt5ycRGc3M15aD4DS4ryTvH6tmEFAgEVx8VQGtbg3~pQigHd-U7BGFr-rzSskag-ZrFo5aYlhvrV3Rfbb0PKXEDdsoQ0V-oXG2x8ovip9FOQb67IRpeD27ImWq65oTcKRI0eEuA~yF6kmKOiL1OknJh5xnp6FB-TU2~aMETqfWU0xOmQnbZyT0xXeLAP0HV7b~Gl6LLkYbgtkUuRKZlREImHUvSOrbgUD-1E2spxPJmROo2Vpro8uD81f4icfM7pvl9XWJYz~A__" alt="" />
           </div>
           <div className='date'>
             23/2/2002
--- Univibe/src/component/Content/Content2.jsx
+++ Univibe/src/component/Content/Content2.jsx
@@ -10,7 +10,7 @@ export default function Content2() {
       <div className='allcard'>
         <div className='card'>
           <div className='photo'>
-            <img src="https://placehold.co/250x400" alt="" />
+            <img src="https://s3-alpha-sig.figma.com/img/da85/7b7e/895e9cf45921b0a7677dabff3fdf8562?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=gIOREt4-z4UJKiXqv9skH8-~Vrgxs1Vetpzx27tmCSYRZMHwLK6QuEwcwcnKjXlSe8FYXNYNAWL3T-MRLvv-pY5fe-CCuNewEb0P1E4N77zCMUzWnrDH6JEZjYjN-J-gH3SlbTMvgYxf2OhgC4v7hxoviGjBOzN2idWB1I5~TCiaM4g7nwsZbvNXAJKv7F3eR6JWdNydf9QENHwITOs4E3jLTp8gAlHvMCRkLNURuWnv0WiP9K0BhHT-nRZ4JdYfzA385v2384xXOUErO0Md0zHym~T-qUwmLyZQpvH37wQdDB0a~QIJRX0S43Z85NDf7O-esmB686YiCJLj~fzWFg__" alt="" />
           </div>
           <div className='date'>
             23/2/2002
@@ -24,7 +24,7 @@ export default function Content2() {
         </div>
         <div className='card'>
         <div className='photo'>
-            <img src="https://placehold.co/250x400" alt="" />
+            <img src="https://s3-alpha-sig.figma.com/img/95cd/8fef/e0a6d6d182c0e174d6260ca0aee19b7b?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=mKGk3ZWkBNhKW0k23r1ysHVe~5f82YYS4GfiFzR-7shjd4yGKoX2yNs8rjKtoo6Wyic1n37xXFgC6WXxaBj-Lu3C1uPGZl3nxxnwTrwzg1xhLwCiYJ~J6jcC6tjyiTgIwAUuSZ9-LJWlzpLiSWz4NQSW0zwP05Zwk2xyXv64Qg7bO1nv~RChPz8abi9sbuYpvoJJT9063q0N44mVoknOfbFG2QCRJXqGDK6SkZWB-EjGUJumdFK-q6nklchO1Bwe6LX7UUJSvNR3LJ6Da1OOVcfXEc~9EYAN-LQogLUa2P6MphtlRKuhT2cF7hQAZL6Zj3XeBEy4PPvsxW1W4ZVgXQ__" alt="" />
           </div>
           <div className='date'>
             23/2/2002
@@ -38,7 +38,7 @@ export default function Content2() {
         </div>
         <div className='card'>
         <div className='photo'>
-            <img src="https://placehold.co/250x400" alt="" />
+            <img src="https://s3-alpha-sig.figma.com/img/f9f0/4b46/78de848caead04e9cda5a391fb7dd268?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=qr7y4r-6yYD0QmyZ0D8siMTPmi6hUQ7NGPM1OhXN5vLCxHOQWlojs73hxALvbDs-2Dekx4x6jt41PsO08LXC1uzKwWpi26bU98CNOaGn9-aCu4c4UqsLZVZ~~VMONyd9apN1~vrtGwIBFP7sAfewST45h2zg0s4Xk4u36y0zsdXfkV64rBrSFLDWLm~1czxQUcDt~0Wsy7mNVOayQfZLRRHiWcQq7WnPfyWty7dxjNTLFYmcOwo40j02uNeSnee77SMmrhMnB9UxZtBqgzWUvUUt-CKP3MDOZddIPQ0QGh39cMV16FSr9UOe56MWJB3bU3T06v~I-EAxyrDixhBHyw__" alt="" />
           </div>
           <div className='date'>
             23/2/2002
@@ -52,7 +52,7 @@ export default function Content2() {
         </div>
         <div className='card'>
         <div className='photo'>
-            <img src="https://placehold.co/250x400" alt="" />
+            <img src="https://s3-alpha-sig.figma.com/img/b319/2a1c/5c7e4eda1ebf25fc685b703fd76af666?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=lFO3nyt9MXpfFmj6PukQiXg-SJF99qfL4Be6cvlBmaXeT9XYBuuGloMM9IZy9~kVPLcB9CGPGZ~YBey2~BHtzcatl4d9cOuS-NBRzEepAVevh4YHaenohHYBSba1lEOASmdGgxSSbiIfeOSRx1WBf21kv1243pict6eogTl7hMSqhvuEY~XWLxKcGHlec4ELg8T0lsd6mIreMXR8-sIeYR9t6uJQNtKG29oyQvHpcNDoML1LX-x7m98zs8NG4EuY22lHsxb1s7RzpYd2IT23Pwu5sDsk7zRL8LeXGw1W8CEEni4NxEwU8Xlwpw~rhFNzAe-QBu-~RxxQRjsovueReQ__" alt="" />
           </div>
           <div className='date'>
             23/2/2002
@@ -66,7 +66,7 @@ export default function Content2() {
         </div>
         <div className='card'>
         <div className='photo'>
-            <img src="https://placehold.co/250x400" alt="" />
+            <img src="https://s3-alpha-sig.figma.com/img/fe0d/f673/83f59047861a406376d5fbb76e199442?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=gW9QtQoLaIAK-2gTT95-BKgpid7CZFoXGWYJxloAyhcu28XN99ue-DyzQaNLmcedWCSdtHKVBzVv6mzuNZZuCjfLkirISYEEWhrg5IzHH8t5ebzsiPxY2NlN-QJMMFFk0h7LwafHC3e4SZOHGa~h4-OO~xkk-g8KqlpRiZwcxgau4byOdPLEP2QbG08t0dxbsTArrNGgILfQb9dWL3v5ORvmA0sMF2zk57RLlnJIr-zQUR9hJL4adaHr~uiMQyOFnsBAhn5QVmFWbsQMWX~tk95fqw52mJt4o-t-cyFcMGm~IE2OlmB2NIJknftaG6EO26vWoxi5HvlMiN5BiXfvxw__" alt="" />
           </div>
           <div className='date'>
             23/2/2002
@@ -80,7 +80,7 @@ export default function Content2() {
         </div>
         <div className='card'>
         <div className='photo'>
-            <img src="https://placehold.co/250x400" alt="" />
+            <img src="https://s3-alpha-sig.figma.com/img/3709/a164/08154e98caa44b0181ad92512842a4d6?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=JM-56~uTR6-Q2uVyudaIMqiG0jDyM7UBdORlbb7w9vhdLJ82L1fWahsArFdNgrF4ANIiPO7~nT5rtF9lyqCHujJLLxaL1WLK5atgoWLo6Bzf6zpXafWFWFTZHJudx5t5EzuYf99SjplATCj2eibzqZKVmYsRA6rgpZmVcIvTV3r~JG8GmmFW2NWLitXi32jzhqCUEu0~wuzo-Re8nEwnhOQYE2uCWW0Tb9rZm5GwCvwarDoGFpYiqpUlSWgpkj7LN1megMzghk106K1RDEIW6IAd82pZ2POHvcG9Z7f4wBSO~fHp1FXEmyxlb412f2nFqq9GP--MaS9dWevlWyRtFg__" alt="" />
           </div>
           <div className='date'>
             23/2/2002
